66a8e72f7079ab2bc4122cbc36b728f532c091b3
Kenpari <wyattkieffner@gmail.com>
2023-03-13 15:18:36 -0700

    Changes

--- bot.py
+++ bot.py
@@ -1,11 +1,15 @@
 import json
 import os
+import re
 import dotenv
 import discord
 import requests
 
 #create bot class
 bot = discord.Bot()
+
+#add command group for workshop mod monitoring-related commands
+mods = bot.create_group("mods", "Commands related to monitored Workshop mods")
 
 #add a ready event for when the bot is logged in and active
 @bot.event
@@ -21,35 +25,56 @@ with open("workshop_items.json", 'r', encoding='utf-8') as f:
     except json.JSONDecodeError:
         print("Unable to load workshop data from file.")
 
-@bot.slash_command(name='add', description="Add Steam workshop IDs to a list of monitored mods.")
-async def add_workshop_ids(ctx, *workshop_ids: int):
+@mods.command(
+        name='add',
+        description="Add Steam workshop IDs to a list of monitored mods.",
+        options = [
+            discord.Option(input_type = str,
+                           name = "workshop_ids",
+                           description="List of Workshop IDs to add to the monitored list, with spaces between each ID.")
+        ])
+async def add_workshop_ids(ctx, workshop_ids: str):
     """Slash command to add workshop IDs from a space-separated list of user-provided IDs
       to a JSON to be monitored"""
-    for workshop_id in workshop_ids:
-        if str(workshop_id) in workshop_data:
-            continue
-        workshop_data[str(workshop_id)] = ""
-        with open('workshop_items.json', 'w', encoding='utf-8') as f:
-            json.dump(workshop_data, f)
-    await ctx.send("Workshop IDs added to the list.")
+    try:
+        assert re.match(r"^(?:\d\s?)*$", workshop_ids)
+        for workshop_id in workshop_ids.split():
+            if workshop_id in workshop_data:
+                continue
+            workshop_data[workshop_id] = ""
+            with open('workshop_items.json', 'w', encoding='utf-8') as f:
+                json.dump(workshop_data, f)
+        await ctx.respond("Workshop IDs added to the list.")
+    except AssertionError:
+        await ctx.respond("There was a problem processing the provided list of IDs. Please make sure you are using the correct format.")
 
-@bot.slash_command(name='remove',
-                   description="Remove Steam workshop IDs to a list of monitored mods.")
-async def remove_workshop_ids(ctx, *workshop_ids: int):
+@mods.command(
+        name='remove',
+        description="Remove Steam workshop IDs to a list of monitored mods.",
+        options = [
+            discord.Option(input_type = str,
+                           name = "workshop_ids",
+                           description="List of Workshop IDs to remove from the monitored list, with spaces between each ID.")
+        ])
+async def remove_workshop_ids(ctx, workshop_ids: str):
     """Slash command to remove workshop IDs from a space-separated list of user-provided
     IDs from the monitored JSON"""
-    in_list = False
-    for workshop_id in workshop_ids:
-        if str(workshop_id) not in workshop_data:
-            continue
-        del workshop_data[str(workshop_id)]
-        in_list = True
-    if in_list:
-        with open('workshop_items.json', 'w', encoding='utf-8') as f:
-            json.dump(workshop_data, f)
-        await ctx.send("Workshop IDs removed from the list.")
-    else:
-        await ctx.send("Given IDs were not found in the list.")
+    try:
+        assert re.match(r"^(?:\d\s?)*$", workshop_ids)
+        in_list = False
+        for workshop_id in workshop_ids.split():
+            if workshop_id not in workshop_data:
+                continue
+            del workshop_data[workshop_id]
+            in_list = True
+        if in_list:
+            with open('workshop_items.json', 'w', encoding='utf-8') as f:
+                json.dump(workshop_data, f)
+            await ctx.respond("Workshop IDs removed from the list.")
+        else:
+            await ctx.respond("Given IDs were not found in the list.")
+    except AssertionError:
+        await ctx.respond("There was a problem processing the provided list of IDs. Please make sure you are using the correct format.")
 
 def get_update_dates():
     """Function to check update dates of mods in monitored JSON"""
@@ -66,7 +91,6 @@ def get_update_dates():
             try:
                 last_updated = pubfile["time_updated"]
                 recorded_update = workshop_data[workshop_id]
-                print(recorded_update)
                 if recorded_update != '':
                     if recorded_update < last_updated:
                         updated.append(workshop_id)
@@ -79,8 +103,7 @@ def get_update_dates():
         json.dump(workshop_data, f)
     return updated
 
-#Add command to check for updates for tracked mods
-@bot.slash_command(name="checkupdates",
+@mods.command(name="checkupdates",
                    description="Check for updates to monitored Workshop items.")
 async def check_updates(ctx):
     """Slash command to manually check for updates in monitored JSON"""
@@ -89,6 +112,16 @@ async def check_updates(ctx):
         await ctx.respond(f"Found updates for Workshop IDs: {', '.join(updated)}")
     else:
         await ctx.respond("No updates found! Smooth sailing!")
+
+@mods.command(name="list",
+                   description="View a list of all currently monitored workshop IDs.")
+async def print_list(ctx):
+    """Slash command to print a list of all currently monitored workshop IDs"""
+    if workshop_data:
+        await ctx.respond(
+            f"All currently monitored Workshop IDs:\n{', '.join([i for i in workshop_data])}")
+    else:
+        await ctx.respond("No currently monitored items.")
 
 #login with the bot's token
 dotenv.load_dotenv()
